c1529962e1eef649f66433ec27df8f9734673738
wahel31102005 <wahelsianturi684@gmail.com>
2024-07-17 10:36:49 +0700

    Add files via upload

---
+++ LifestyleStore/connection.php
@@ -0,0 +1,4 @@
+<?php
+//$con=mysqli_connect("localhost","id2538044_sajalagrawal","","") or die(mysqli_error($con));
+$con=mysqli_connect("localhost","root","","store") or die(mysqli_error($con));
+?>
